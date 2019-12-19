f4da121a6d61e34d747f6a18af855eb9b3645653
Ramakrishna Pydimarla <rampydim@publicisgroupe.net>
2019-12-19 16:26:14 +0530

    new change is added to script

--- git-branches-by-commit-date.sh
+++ git-branches-by-commit-date.sh
@@ -1,1 +1,3 @@
 for branch in `git branch -r | grep -v HEAD`;do echo -e `git show --format="%ci %cr" $branch | head -n 1` \\t$branch; done | sort -r
+
+#git branch -r | grep -v HEAD | while read b; do git log --color --format="%ci _%C(magenta)%cr %C(bold cyan)$b%Creset %s %C(bold blue)<%an>%Creset" $b | head -n 1; done | sort -r | cut -d_ -f2- | sed 's;origin/;;g' | head -10
